c2876e6c74f31b13566310a75dc3a6b833b41387
ELmmostaineMohammed <mohammedelmostaine@gmail.com>
2025-05-11 22:10:41 +0100

    Remove Slack configuration from services.php

--- your-dentist/config/services.php
+++ your-dentist/config/services.php
@@ -28,11 +28,4 @@ return [
         'key' => env('RESEND_KEY'),
     ],
 
-    'slack' => [
-        'notifications' => [
-            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
-            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
-        ],
-    ],
-
 ];
